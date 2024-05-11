392ca57ddec6797cef88bcbd7b731c8f97e6bc15
Droopytex <a.velasquezvega@gmail.com>
2024-05-11 14:43:26 -0400

    configuración de componente para mostrar Api

---
+++ react_api_shinobi/src/App.jsx
@@ -0,0 +1,15 @@
+import Mi_Api from "./componentes/mi_api/Mi_Api";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hola todos, aquí vamos de nuevo</h1>
+      <div>
+        <Mi_Api />
+      </div>
+    </>
+  );
+}
+
+export default App;
